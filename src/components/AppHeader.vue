566cbd72ab1ab5f5c0b1a1208d05d2e600c000aa
Nino-design <npompili@gmail.com>
2022-05-23 16:25:37 +0200

    init

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="header">
+<input type="text">
+<button>Invia</button>
+</div>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,12 @@
+<template>
+<div>cwrfcq</div>
+</template>
+
+<script>
+export default {
+  name: "AppMain",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
